29da2fc84c24b2eb04d982fda1e81fc1dd027201
mohamedkamal333 <mksalem614@gmail.com>
2022-05-01 13:44:08 +0200

    second commit

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -11,115 +11,128 @@ import cardWidth from "./img/card/المعايدة العرضية.png";
 import "./home.scss";
 
 function Home() {
-    
-    
-    
-    
-    async function saveBlobAsFile(url, fileName) {
-        var reader = new FileReader();
-    
-        const blob = await fetch(url).then(res => res.blob())
-        reader.onloadend = function () {    
-            var base64 = reader.result ;
-            var link = document.createElement("a");
-    
-            document.body.appendChild(link); // for Firefox
-    
-            link.setAttribute("href", base64);
-            link.setAttribute("download", fileName);
-            link.click();
-        };
-        reader.readAsDataURL(blob);
-    };
+  return (
+    <section className="main-home">
+      <div className="container">
+        <div className="show-img-from-top-right">
+          <img src={positionTopImg} alt="img" />
+        </div>
+        <div className="pr-item">
+          <div className="it-img-top">
+            <img src={topTitleImage} alt="عيدكم مبارك" />
+          </div>
+          <div className="about-text">
+            <p>
+              نتقدم لكم بأسمى التهاني والتبريكات بمناسبة حلول عيد الفطر المبارك
+              تقبل الله منا ومنكم صالح الأعمال و نسعد بتقديم تصميم التهنئة بهوية
+              أستروفيل.
+            </p>
+          </div>
 
-
-    // name value input 
-
-
-
-
-
-
-    return (
-        <section className="main-home">
-            <div className="container">
-                <div className="show-img-from-top-right">
-                    <img src={positionTopImg} alt="img"/>
+          <div className="cho-img">
+            <p>فضلًا اختر حجم الصورة المطلوب</p>
+            <div className="one-tow">
+              <div className="wid-item">
+                <div className="bacgo-wi"></div>
+                <div className="txt-down">
+                  <h4>عرضي</h4>
+                  <span></span>
                 </div>
-                <div className="pr-item">
-                    <div className="it-img-top">
-                        <img src={topTitleImage} alt="عيدكم مبارك"/>
-                    </div>
-                    <div className="about-text">
-                        <p>
-نتقدم لكم بأسمى التهاني والتبريكات بمناسبة حلول عيد الفطر المبارك 
-تقبل الله منا ومنكم صالح الأعمال
-و نسعد بتقديم تصميم التهنئة بهوية أستروفيل.
-                        </p>
-                    </div>
-
-                    <div className="cho-img">
-                        <p>فضلًا اختر حجم الصورة المطلوب</p>
-                        <div className="one-tow">
-                            <div className="wid-item">
-                                <div className="bacgo-wi">
-
-                                </div>
-                                <div className="txt-down">
-                                    <h4>عرضي</h4>
-                                    <span></span>
-                                </div>
-                            </div>
-                            <div className="hei-item">
-                            <div className="bacgo-hei">
-                                </div>
-                                <div className="txt-down">
-                                    <h4>طولي</h4>
-                                    <span></span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* Create the card */}
-
-                    <div className="create-card">
-                    <div className="add-name">
-                    <input type="name" name="name" id="getName" placeholder="الأسم"/>
-                    </div>
-                        <div className="creat">
-                        <button  onClick={() => saveBlobAsFile(`${cardWidth}`, `${document.getElementById("getName").value}`)}>إنشاء البطاقة</button>
-                        </div>
-                        <div className="img-ha">
-                        <a href="http://twitter.com/FalakKsa" target="_blank">
-                        <span>بدعم من</span>
-                        <img src={footerImage} alt="يدعم من"/>
-                        </a>
-                        </div>
-                    </div>
-
-
+              </div>
+              <div className="hei-item">
+                <div className="bacgo-hei"></div>
+                <div className="txt-down">
+                  <h4>طولي</h4>
+                  <span></span>
                 </div>
-
-
-                <div className="img-from-bottom-right">
-                    <div className="img-top">
-                        <img src={positionDownImg} alt="down image"/>
-                    </div>
-                    <div className="social">
-                        <h4>انتسب إلينا</h4>
-                        <ul>
-                            <li> <a href="https://www.youtube.com/channel/UChaP5I6GcKi8MnfIGbT9UOA" target="_blank"><img src={iconYout} alt="youtube"/></a> </li>
-                            <li> <a href="https://t.me/Ar_Astrophile" target="_blank"><img src={iconTelegram} alt="telegram"/></a> </li>
-                            <li> < a href="https://mobile.twitter.com/astrophile_ar" target="_blank"><img src={iconTwiter} alt="twitter"/></a> </li>
-                        </ul>
-                    </div>
-                </div>
-
-
+              </div>
             </div>
-        </section>
-    )
+          </div>
+
+          {/* Create the card */}
+
+          <div className="create-card">
+            <div className="add-name">
+              <input type="name" name="name" id="getName" placeholder="الأسم" />
+            </div>
+            <div className="creat">
+              <button>إنشاء البطاقة</button>
+            </div>
+            <div className="img-ha">
+              <a href="http://twitter.com/FalakKsa" target="_blank">
+                <span>بدعم من</span>
+                <img src={footerImage} alt="يدعم من" />
+              </a>
+              <div className="social">
+                <h4>انتسب إلينا</h4>
+                <ul>
+                  <li>
+                    {" "}
+                    <a
+                      href="https://www.youtube.com/channel/UChaP5I6GcKi8MnfIGbT9UOA"
+                      target="_blank"
+                    >
+                      <img src={iconYout} alt="youtube" />
+                    </a>{" "}
+                  </li>
+                  <li>
+                    {" "}
+                    <a href="https://t.me/Ar_Astrophile" target="_blank">
+                      <img src={iconTelegram} alt="telegram" />
+                    </a>{" "}
+                  </li>
+                  <li>
+                    {" "}
+                    <a
+                      href="https://mobile.twitter.com/astrophile_ar"
+                      target="_blank"
+                    >
+                      <img src={iconTwiter} alt="twitter" />
+                    </a>{" "}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="img-from-bottom-right">
+          <div className="img-top">
+            <img src={positionDownImg} alt="down image" />
+          </div>
+          <div className="social">
+            <h4>انتسب إلينا</h4>
+            <ul>
+              <li>
+                {" "}
+                <a
+                  href="https://www.youtube.com/channel/UChaP5I6GcKi8MnfIGbT9UOA"
+                  target="_blank"
+                >
+                  <img src={iconYout} alt="youtube" />
+                </a>{" "}
+              </li>
+              <li>
+                {" "}
+                <a href="https://t.me/Ar_Astrophile" target="_blank">
+                  <img src={iconTelegram} alt="telegram" />
+                </a>{" "}
+              </li>
+              <li>
+                {" "}
+                <a
+                  href="https://mobile.twitter.com/astrophile_ar"
+                  target="_blank"
+                >
+                  <img src={iconTwiter} alt="twitter" />
+                </a>{" "}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Home
+export default Home;
